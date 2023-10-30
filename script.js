6bda088eb686c0c50fe5f859e4aa8d770d08aca2
wolf-ed <wolf7ed@gmail.com>
2023-10-30 18:34:24 +0100

    index and script

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const widthParagraph = document.getElementById('widthId');
+const heightParagraph = document.getElementById('heightId');
 const btn = document.getElementById('btn');
-
-console.log('works' + window.innerWidth);
 
 btn.addEventListener('click', () => {
   widthParagraph.innerHTML = window.innerWidth;
