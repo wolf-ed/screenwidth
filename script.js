6303e76de3b784bbe61eabb1aa307f0ffc09eaa6
wolf-ed <wolf7ed@gmail.com>
2023-10-30 20:03:52 +0100

    add height and improve style

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const widthParagraph = document.getElementById('widthId');
 const heightParagraph = document.getElementById('heightId');
 const btn = document.getElementById('btn');
 
+widthParagraph.innerHTML = window.innerWidth;
+heightParagraph.innerHTML = window.innerHeight;
+
 btn.addEventListener('click', () => {
   widthParagraph.innerHTML = window.innerWidth;
+  heightParagraph.innerHTML = window.innerHeight;
 });
